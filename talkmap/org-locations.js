4e3d78bd28b5e060b409a5b94c0134baaa1d87d4
Alessandro Sebastianelli <33869842+alessandrosebastianelli@users.noreply.github.com>
2022-11-04 12:29:45 +0100

    New locations for talks

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -8,5 +8,25 @@ var addressPoints = [
     "Online",
     0.0,
     -24.140044
+  ],
+  [
+    "Brussels, Belgium",
+    50.826040,
+    4.380211
+  ],
+  [
+    "Benevento, Italy",
+    41.203563,
+    14.752040
+  ],
+  [
+    "Padova, Italy",
+    45.404632,
+    11.878040
+  ],
+  [
+    "Lecce, Italy",
+    40.351515,
+    18.175016
   ]
 ];
